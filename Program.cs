5763578b528f96c377ac8c956f9a85360b76e814
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-24 10:02:26 +0300

    Toteutus tehty

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+
+namespace RestfulSyksy2025;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            await GetItemsAsync(client);
+        }
+    }
+
+    static async Task GetItemsAsync(HttpClient client)
+    {
+        var response = await client.GetAsync("https://api.restful-api.dev/objects");
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(json);
+            return;
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+            return;
+        }
+    }
+}
